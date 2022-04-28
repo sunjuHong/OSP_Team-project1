2b870200d7e4f9351711107d7e17c07d6a125d8f
kimjihun2 <wlgns5674@naver.com>
2022-04-28 20:22:20 +0900

    setFt.py is updated

--- setFt.py
+++ setFt.py
@@ -1,26 +1,29 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sys
 
-#1. input integer list
-print("input the 1st list: ",end='')
-listA=input()
-print("input the 2nd list: ",end='')
-listB=input()
+def setFt():
+        #1. input integer list
+        print("input the 1st list: ",end='')
+        listA=input()
+        print("input the 2nd list: ",end='')
+        listB=input()
 
-#2. convert list into set
-setA=map(int,listA.split())
-setB=map(int,listB.split())
-setA=set(setA)
-setB=set(setB)
+        #2. convert list into set
+        setA=map(int,listA.split())
+        setB=map(int,listB.split())
+        setA=set(setA)
+        setB=set(setB)
 
-#3. set union, intersection
-setUnion=setA.union(setB)
-setIntersection=setA.intersection(setB)
+        #3. set union, intersection
+        setUnion=setA.union(setB)
+        setIntersection=setA.intersection(setB)
 
-#4. convert set into list
-setUnion=list(setUnion)
-setIntersection=list(setIntersection)
+        #4. convert set into list
+        setUnion=list(setUnion)
+        setIntersection=list(setIntersection)
+
+        print("union: ",setUnion)
+        print("intersection: ",setIntersection)
 
 if __name__=='__main__':
-	print("union: ",setUnion)
-	print("intersection: ",setIntersection)
+        setFt()
